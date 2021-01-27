b037725605085ed37bf052441e98e764d789a736
Victorya01 <vika-sann@list.ru>
2021-01-27 13:26:10 +0300

    Добавьте файлы проекта.

---
+++ по дииагонали/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        char[,] A = new char[5, 5];
+        A[0, 0] = '*';
+        A[1, 1] = '*';
+        A[2, 2] = '*';
+        A[3, 3] = '*';
+        A[4, 4] = '*';
+
+        for (int i = 0; i < A.GetLength(0); i++)
+        {
+            for (int j = 0; j < A.GetLength(1); j++)
+            {
+
+
+                if (i == j)
+                {
+                    Console.Write(A[i, j]);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
